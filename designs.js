50e4627aa5fc66b7bc8d6b52bf5c12c92de89a5c
Andrew Huschka <huschkaa@gmail.com>
2017-10-22 20:57:22 -0500

    grid input working

--- designs.js
+++ designs.js
@@ -1,16 +1,28 @@
 // Select color input
-
-// Select size input
+$('#colorPicker').on('click',function colorSelect() {
+  const color = $('#colorPicker').val();
+  $('.color_changing').css("background-color", color);
+});
 
 // When size is submitted by the user, call makeGrid()
 
-$('#submit').on('click',function makeGrid() {
+$('#submit').on('click',function makeGrid(click) {
+  click.preventDefault()
+  $('#pixel_canvas').find('tr').remove();
+  $('#pixel_canvas').find('td').remove();
+
   const n = $('#input_height').val();
   const m = $('#input_width').val();
-  const canvas = $('#pixel_canvas');
-  const square = $('<td></td>');
-  const row = $('<tr></tr>');
-  for (var x = 0; x < n; x++) {
-      canvas.append(square);
+
+  for (var x = 1; x <= n; x++) {
+    $('#pixel_canvas').append('<tr></tr>')
+  }
+  for (var y = 1; y <= m; y++) {
+    $('tr').append('<td></td>')
   }
 });
+
+//color the canvas when it is clicked
+$('td').on('click', function() {
+  $(this).toggleClass("color_changing");
+} );

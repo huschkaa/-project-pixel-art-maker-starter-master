c9bfecfe04027f214f99a6a8480cb2ef78d57aab
Andrew Huschka <huschkaa@gmail.com>
2017-10-26 19:45:01 -0500

    Final update after passing Udacity requirements

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ## Table of Contents
 
-* [1) Choose grid height and grid width (between 1 and 50 integer).
+*  1) Choose grid height and grid width (between 1 and 50 integer).
    2) Choose color to paint the canvas and click the squares.
-   3) Change color as you would like and finish coloring the canvas.](#instructions)
+   3) Change color as you would like and finish coloring the canvas.(#instructions)
 
-* [Andrew Huschka](#contributing)
+* Andrew Huschka(#contributing)
 
 ## Instructions
 
 To get started, open `designs.js` and start building out the app's functionality.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+For specific, detailed instructions, look at the project instructions in the (https://classroom.udacity.com/me).
 
 ## Contributing
 
--- designs.js
+++ designs.js
@@ -1,22 +1,21 @@
 // When size is submitted by the user, call makeGrid()
 
 $('#submit').on('click',function makeGrid(click) {
-  click.preventDefault()
+  click.preventDefault();
 
 //Erase the previous grid to put in a new grid
   $('#pixel_canvas').find('tr').remove();
   $('#pixel_canvas').find('td').remove();
 
 //create the new grid that is n x m size
-  const n = $('#input_height').val();
-  const m = $('#input_width').val();
+  const gridHeight = $('#input_height').val();
+  const gridWidth = $('#input_width').val();
 
-  for (var x = 1; x <= n; x++) {
-    $('#pixel_canvas').append('<tr></tr>')
+  for (var x = 1; x <= gridHeight; x++) {
+    $('#pixel_canvas').append('<tr></tr>');
   }
-  for (var y = 1; y <= m; y++) {
-    $('tr').append('<td></td>')
-
+  for (var y = 1; y <= gridWidth; y++) {
+    $('tr').append('<td></td>');
   }
 
 //color the canvas when it is clicked
